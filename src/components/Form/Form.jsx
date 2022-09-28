5efd4f540ba8d4ec75a9fa68a7ce7768e4e64e5e
VPagent <vpagent13@gmail.com>
2022-09-28 16:33:48 +0300

    fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -51,7 +51,7 @@ function Form() {
           className={s.input}
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           value={name}
